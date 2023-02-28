e11bd6756de2856f38f04418a9902ff7f1f2a32f
Tejas Bendkule <74281821+tejas0230@users.noreply.github.com>
2023-02-28 06:41:17 +0530

    added basic Login page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import React from "react";
 import "./App.css";
 import Admin from "./pages/Admin";
 import { Landing } from "./pages/Landing";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <>
-      <Landing/>
+      <Login/>
     </>
       
     
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import { LandingNavbar } from "../components/LandingNavbar";
 import "../styles/login.scss";
-
+import "../styles/landingNavbar.scss";
 const Login = () => {
-  return <div>Login</div>;
+  return <div>
+    <LandingNavbar/>
+    <section>
+      <div className="div-block-2">
+          <h1 className="heading-3">Login</h1>
+          <div className="form">
+            <form>
+              <label for="username" className="field-lable">Username</label>
+              <input type="text" className="input-field" name="name"></input>
+              
+              <label for="password" className="field-lable">Password</label>
+              <input type="password" className="input-field" name="password"></input>
+
+              <input type="submit" value="Login" className="w-button"></input>
+            </form>
+          </div>
+      </div>
+    </section>
+  </div>;
 };
 
 export default Login;
